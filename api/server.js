72119fa2123f5362d006472e0db031641f72c513
Jacob Adlers <jacob.adlers@gmail.com>
2020-05-11 16:08:16 +0200

    Add endpoint to recalculate all ELO ratings

--- api/server.js
+++ api/server.js
@@ -453,6 +453,119 @@ app.get("/league/:leagueId/last-dota-match-id", async (req, res, next) => {
   }
 });
 
+// NOTE: Careful, this should be locked and only available for admins
+app.get("/recalculate-elo-for-all-players", async (req, res, next) => {
+  try {
+    const updatedData = await recalculateEloRatingForAllPlayers();
+    updatedData.sort((a, b) => a.id - b.id);
+    res.status(200).json({ message: "Success!", data: updatedData });
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Nope");
+    next();
+  }
+});
+
+/**
+ * WARNING: this resets the rating for all players
+ * Utility function which should not need to be run more than once.
+ *
+ * Recalculates the ELO rating for each user and updates all matches they've
+ * played with their ELO at the time of playing the match.
+ */
+async function recalculateEloRatingForAllPlayers() {
+  try {
+    const matches = await db.getAllMatchesFromLeague(2); // NOTE: Hardcoded league
+    // Make sure the matches are sorted in order since we're rewriting the ELO
+    // advancments of each player from the beginning
+    matches.forEach((match) => {
+      match.dota_match_id = parseInt(match.dota_match_id);
+    });
+    matches.sort((a, b) => a.dota_match_id - b.dota_match_id);
+
+    // Get data needed for each player: userId, eloRating, matchesPlayed
+    const users = await db.getAllUsers();
+    const playerData = users.map((user) => {
+      return {
+        id: user.id,
+        username: user.username,
+        eloRating: 1500,
+        matchesPlayed: 0,
+      };
+    });
+
+    // Loop over matches.
+    for (const match of matches) {
+      // Needed for each match: playerIds, winningTeamPlayerIds, averageTeamRatings
+      const teamIds = await db.getTeamsInMatch(match.id);
+      const teams = await Promise.all(
+        teamIds.map(async (teamId) => {
+          return { id: teamId, playerIds: await db.getUsersInTeam(teamId) };
+        })
+      );
+
+      const playersIdsInMatch = teams.map((team) => team.playerIds).flat();
+
+      // DB: Add players current ELO to match in user_match_stats
+      await Promise.all(
+        playersIdsInMatch.map((playerId) => {
+          const currentElo = playerData.find((player) => player.id === playerId)
+            .eloRating;
+          return db.setUserEloRatingForMatch(match.id, playerId, currentElo);
+        })
+      );
+
+      // Calculate both teams average ELO (ELO for each player in the team)
+      teams.forEach((team) => {
+        const totalRating = team.playerIds.reduce((acc, playerId) => {
+          const currentPlayerData = playerData.find(
+            (player) => player.id === playerId
+          );
+          return acc + currentPlayerData.eloRating;
+        }, 0);
+        team.averageEloRating = totalRating / team.playerIds.length;
+      });
+
+      let updateUserEloPromises = [];
+      teams.forEach((team) => {
+        // Find out which players are in the winning team
+        const winner = team.id === match.winning_team_id;
+        // Calculate diff for each player
+        team.playerIds.forEach((playerId) => {
+          const currentPlayerInfo = playerData.find(
+            (player) => player.id === playerId
+          );
+          const eloRatingDiff = elo.ratingDiff(
+            team.averageEloRating,
+            teams.filter((t) => t.id !== team.id)[0].averageEloRating,
+            winner,
+            currentPlayerInfo.matchesPlayed
+          );
+          currentPlayerInfo.eloRating += Math.round(eloRatingDiff);
+          currentPlayerInfo.matchesPlayed++;
+          updateUserEloPromises.push(
+            db.setUserEloRating(playerId, currentPlayerInfo.eloRating)
+          );
+        });
+      });
+
+      // DB: Update the ELO for each player in the match
+      await Promise.all(updateUserEloPromises);
+    }
+
+    // Log all users with updated eloRating
+    console.log(
+      playerData
+        .filter((player) => player.matchesPlayed > 0)
+        .sort((a, b) => a.id - b.id)
+    );
+    return playerData;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 // Respond 404 for routes not specified
 app.use((req, res, next) => {
   if (!res.headersSent) {
