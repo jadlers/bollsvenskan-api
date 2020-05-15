88020dbe54740415a7dfdd4e95da196eb963198d
Jacob Adlers <jacob.adlers@gmail.com>
2020-05-15 15:57:26 +0200

    Add shuffle array function

--- api/server.js
+++ api/server.js
@@ -501,6 +501,24 @@ app.post("/league/:leagueId/create-teams", async (req, res, next) => {
   }
 });
 
+// Taken from here: https://bost.ocks.org/mike/shuffle/
+function shuffle(array) {
+  let m = array.length;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    let i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    let t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
 // NOTE: Careful, this should be locked and only available for admins
 app.get("/recalculate-elo-for-all-players", async (req, res, next) => {
   try {
