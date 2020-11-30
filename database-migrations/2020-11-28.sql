fea78f05c16fff238720a215628998ce5c8cb01e
Jacob Adlers <jacob.adlers@gmail.com>
2020-12-01 00:24:21 +0100

    Fix errors in latest sql migration

--- database-migrations/2020-11-28.sql
+++ database-migrations/2020-11-28.sql
@@ -8,8 +8,18 @@ ADD "first_blood_praise" integer NULL;
 COMMENT ON COLUMN "matches"."winning_team_id" IS '';
 COMMENT ON TABLE "matches" IS '';
 
+ALTER TABLE "matches"
+ADD FOREIGN KEY ("first_blood_mock")
+REFERENCES "first_blood_phrases" ("id")
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE "matches"
+ADD FOREIGN KEY ("first_blood_praise")
+REFERENCES "first_blood_phrases" ("id")
+ON DELETE SET NULL ON UPDATE CASCADE;
+
 -- Add the new tables
-CREATE TYPE fb_phrase_type AS ENUM ('mock', 'praise')
+CREATE TYPE fb_phrase_type AS ENUM ('mock', 'praise');
 CREATE TABLE first_blood_phrases (
     id      SERIAL              PRIMARY KEY,
     phrase  VARCHAR             NOT NULL,
@@ -17,7 +27,7 @@ CREATE TABLE first_blood_phrases (
 )
 
 -- Insert the values currently in the database
-INSERT INTO first_blood_phrases (phrase, kind) VALUES
+INSERT INTO first_blood_phrases (phrase, type) VALUES
        ('<name> var sämst', 'mock'),
        ('<name> hade en dålig dag', 'mock'),
        ('<name> fokuserade inte tillräckligt', 'mock'),
@@ -39,49 +49,49 @@ INSERT INTO first_blood_phrases (phrase, kind) VALUES
        ('Legenden <name> fick in sista slaget', 'praise');
 
 -- Map the current values for each match and insert
-UPDATE matches SET  first_blood_mock = 8  WHERE id = 89 ;
-UPDATE matches SET  first_blood_mock = 2  WHERE id = 88 ;
-UPDATE matches SET  first_blood_mock = 1  WHERE id = 87 ;
-UPDATE matches SET  first_blood_mock = 7  WHERE id = 86 ;
-UPDATE matches SET  first_blood_mock = 12 WHERE id = 106;
-UPDATE matches SET  first_blood_mock = 7  WHERE id = 105;
-UPDATE matches SET  first_blood_mock = 15 WHERE id = 108;
-UPDATE matches SET  first_blood_mock = 18 WHERE id = 110;
-UPDATE matches SET  first_blood_mock = 7  WHERE id = 112;
-UPDATE matches SET  first_blood_mock = 15 WHERE id = 111;
-UPDATE matches SET  first_blood_mock = 9  WHERE id = 114;
-UPDATE matches SET  first_blood_mock = 15 WHERE id = 113;
-UPDATE matches SET  first_blood_mock = 2  WHERE id = 116;
-UPDATE matches SET  first_blood_mock = 12 WHERE id = 115;
-UPDATE matches SET  first_blood_mock = 14 WHERE id = 118;
-UPDATE matches SET  first_blood_mock = 17 WHERE id = 119;
-UPDATE matches SET  first_blood_mock = 2  WHERE id = 121;
-UPDATE matches SET  first_blood_mock = 16 WHERE id = 122;
-UPDATE matches SET  first_blood_mock = 1  WHERE id = 124;
-UPDATE matches SET  first_blood_mock = 12 WHERE id = 125;
-UPDATE matches SET  first_blood_mock = 13 WHERE id = 126;
-UPDATE matches SET  first_blood_mock = 11 WHERE id = 127;
-UPDATE matches SET  first_blood_mock = 8  WHERE id = 128;
-UPDATE matches SET  first_blood_mock = 9  WHERE id = 129;
-UPDATE matches SET  first_blood_mock = 13 WHERE id = 130;
-UPDATE matches SET  first_blood_mock = 17 WHERE id = 131;
-UPDATE matches SET  first_blood_mock = 1  WHERE id = 132;
-UPDATE matches SET  first_blood_mock = 11 WHERE id = 133;
-UPDATE matches SET  first_blood_mock = 12 WHERE id = 134;
-UPDATE matches SET  first_blood_mock = 14 WHERE id = 135;
-UPDATE matches SET  first_blood_mock = 13 WHERE id = 141;
-UPDATE matches SET  first_blood_mock = 17 WHERE id = 140;
-UPDATE matches SET  first_blood_mock = 18 WHERE id = 142;
-UPDATE matches SET  first_blood_mock = 5  WHERE id = 143;
-UPDATE matches SET  first_blood_mock = 6  WHERE id = 144;
-UPDATE matches SET  first_blood_mock = 16 WHERE id = 145;
-UPDATE matches SET  first_blood_mock = 2  WHERE id = 146;
-UPDATE matches SET  first_blood_mock = 3  WHERE id = 147;
-UPDATE matches SET  first_blood_mock = 3  WHERE id = 153;
-UPDATE matches SET  first_blood_mock = 9  WHERE id = 152;
-UPDATE matches SET  first_blood_mock = 1  WHERE id = 156;
-UPDATE matches SET  first_blood_mock = 6  WHERE id = 157;
-UPDATE matches SET  first_blood_mock = 18 WHERE id = 158;
-UPDATE matches SET  first_blood_mock = 9  WHERE id = 161;
-UPDATE matches SET  first_blood_mock = 10 WHERE id = 162;
-UPDATE matches SET  first_blood_mock = 10 WHERE id = 163;
+UPDATE matches SET  first_blood_mock = 8 , first_blood_praise = 19 WHERE id = 89 ;
+UPDATE matches SET  first_blood_mock = 2 , first_blood_praise = 19 WHERE id = 88 ;
+UPDATE matches SET  first_blood_mock = 1 , first_blood_praise = 19 WHERE id = 87 ;
+UPDATE matches SET  first_blood_mock = 7 , first_blood_praise = 19 WHERE id = 86 ;
+UPDATE matches SET  first_blood_mock = 12, first_blood_praise = 19 WHERE id = 106;
+UPDATE matches SET  first_blood_mock = 7 , first_blood_praise = 19 WHERE id = 105;
+UPDATE matches SET  first_blood_mock = 15, first_blood_praise = 19 WHERE id = 108;
+UPDATE matches SET  first_blood_mock = 18, first_blood_praise = 19 WHERE id = 110;
+UPDATE matches SET  first_blood_mock = 7 , first_blood_praise = 19 WHERE id = 112;
+UPDATE matches SET  first_blood_mock = 15, first_blood_praise = 19 WHERE id = 111;
+UPDATE matches SET  first_blood_mock = 9 , first_blood_praise = 19 WHERE id = 114;
+UPDATE matches SET  first_blood_mock = 15, first_blood_praise = 19 WHERE id = 113;
+UPDATE matches SET  first_blood_mock = 2 , first_blood_praise = 19 WHERE id = 116;
+UPDATE matches SET  first_blood_mock = 12, first_blood_praise = 19 WHERE id = 115;
+UPDATE matches SET  first_blood_mock = 14, first_blood_praise = 19 WHERE id = 118;
+UPDATE matches SET  first_blood_mock = 17, first_blood_praise = 19 WHERE id = 119;
+UPDATE matches SET  first_blood_mock = 2 , first_blood_praise = 19 WHERE id = 121;
+UPDATE matches SET  first_blood_mock = 16, first_blood_praise = 19 WHERE id = 122;
+UPDATE matches SET  first_blood_mock = 1 , first_blood_praise = 19 WHERE id = 124;
+UPDATE matches SET  first_blood_mock = 12, first_blood_praise = 19 WHERE id = 125;
+UPDATE matches SET  first_blood_mock = 13, first_blood_praise = 19 WHERE id = 126;
+UPDATE matches SET  first_blood_mock = 11, first_blood_praise = 19 WHERE id = 127;
+UPDATE matches SET  first_blood_mock = 8 , first_blood_praise = 19 WHERE id = 128;
+UPDATE matches SET  first_blood_mock = 9 , first_blood_praise = 19 WHERE id = 129;
+UPDATE matches SET  first_blood_mock = 13, first_blood_praise = 19 WHERE id = 130;
+UPDATE matches SET  first_blood_mock = 17, first_blood_praise = 19 WHERE id = 131;
+UPDATE matches SET  first_blood_mock = 1 , first_blood_praise = 19 WHERE id = 132;
+UPDATE matches SET  first_blood_mock = 11, first_blood_praise = 19 WHERE id = 133;
+UPDATE matches SET  first_blood_mock = 12, first_blood_praise = 19 WHERE id = 134;
+UPDATE matches SET  first_blood_mock = 14, first_blood_praise = 19 WHERE id = 135;
+UPDATE matches SET  first_blood_mock = 13, first_blood_praise = 19 WHERE id = 141;
+UPDATE matches SET  first_blood_mock = 17, first_blood_praise = 19 WHERE id = 140;
+UPDATE matches SET  first_blood_mock = 18, first_blood_praise = 19 WHERE id = 142;
+UPDATE matches SET  first_blood_mock = 5 , first_blood_praise = 19 WHERE id = 143;
+UPDATE matches SET  first_blood_mock = 6 , first_blood_praise = 19 WHERE id = 144;
+UPDATE matches SET  first_blood_mock = 16, first_blood_praise = 19 WHERE id = 145;
+UPDATE matches SET  first_blood_mock = 2 , first_blood_praise = 19 WHERE id = 146;
+UPDATE matches SET  first_blood_mock = 3 , first_blood_praise = 19 WHERE id = 147;
+UPDATE matches SET  first_blood_mock = 3 , first_blood_praise = 19 WHERE id = 153;
+UPDATE matches SET  first_blood_mock = 9 , first_blood_praise = 19 WHERE id = 152;
+UPDATE matches SET  first_blood_mock = 1 , first_blood_praise = 19 WHERE id = 156;
+UPDATE matches SET  first_blood_mock = 6 , first_blood_praise = 19 WHERE id = 157;
+UPDATE matches SET  first_blood_mock = 18, first_blood_praise = 19 WHERE id = 158;
+UPDATE matches SET  first_blood_mock = 9 , first_blood_praise = 19 WHERE id = 161;
+UPDATE matches SET  first_blood_mock = 10, first_blood_praise = 19 WHERE id = 162;
+UPDATE matches SET  first_blood_mock = 10, first_blood_praise = 19 WHERE id = 163;
