30405880186e1340b0253104d8c45af50c1f6d0d
Jacob Adlers <jacob.adlers@gmail.com>
2021-01-10 14:19:32 +0100

    Add authorized state to logs

--- api/src/middleware/logging.js
+++ api/src/middleware/logging.js
@@ -4,8 +4,15 @@ morgan.token("post-body", (req, _) =>
   req.method === "POST" ? JSON.stringify(req.body) : ""
 );
 
-const logger = morgan("[:date[iso]] :status :method :url :post-body", {
-  skip: (req, _) => req.path === "/metrics",
-});
+morgan.token("authorized-user", (req, _) =>
+  req.authorizedUser ? `authUser(${req.authorizedUser})` : "noAuth"
+);
+
+const logger = morgan(
+  "[:date[iso]] :status :method :url :authorized-user :post-body",
+  {
+    skip: (req, _) => req.path === "/metrics",
+  }
+);
 
 export default logger;
